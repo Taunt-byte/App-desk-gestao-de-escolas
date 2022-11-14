af8f2d7449574e88c293728c7d6e19751f34f2c6
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2022-11-14 13:23:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 ## É um aplicação que permite a montagem de horarios com dias,diciplinas e professores dessas diciplinas tudo feito em python.d
-print("Bem vindo ao App criador de horarios para professores")
+#import das bibliotecas
+from tkinter import *
+from tkinter import messagebox
 
-def hello():
-	nome = input("Digite seu nome: ")
-	print("seja bem vindo: ",nome,)
+# Criador da Janela
 
-def diciplinas():
-    dic = input("Digite a sua diciplina")
-    resposta = input("Voce deseja escolher adicionar outra diciplina Sim(1) Não (2)")
-    if (resposta == 2): 
-        dic2 = input("Digite a sua diciplina")
-    else:
-       	print("")
-hello()
-diciplinas()
-    
+jan = Tk()
+jan.title("Sistema de montagem de horarios")
+jan.geometry("600x300")
+jan.configure(background="black")
+jan.resizable(width=False,height=False)
+ 
+jan.mainloop()   
